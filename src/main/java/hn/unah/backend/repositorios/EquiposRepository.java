0b3f904082ae5ff38e9c943d2b7bed4369f268e1
Diana-Duron <72368042+Diana-Duron@users.noreply.github.com>
2023-11-22 03:33:02 -0600

    CRUD equipos

---
+++ src/main/java/hn/unah/backend/repositorios/EquiposRepository.java
@@ -0,0 +1,9 @@
+package hn.unah.backend.repositorios;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import hn.unah.backend.modelos.Equipos;
+
+public interface EquiposRepository extends JpaRepository<Equipos, Integer>{
+    
+}
