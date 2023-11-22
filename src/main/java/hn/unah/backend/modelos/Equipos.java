1d17cef91eeec1e82675ad952c29356788e90739
Diana-Duron <72368042+Diana-Duron@users.noreply.github.com>
2023-11-22 03:28:56 -0600

    CRUD equipos

---
+++ src/main/java/hn/unah/backend/modelos/Equipos.java
@@ -0,0 +1,36 @@
+package hn.unah.backend.modelos;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name="equipos")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Equipos {
+    
+    @Id
+    @Column(name="idequipos")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idEquipo;
+
+    private String nombre;
+
+    @Column(name="aniofundacion")
+    private String anioFundacion;
+
+    private String pais;
+
+    private String escudo;
+
+}
